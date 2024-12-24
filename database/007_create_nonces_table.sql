404a8022c2792d34a856f54b3ac3a96193169654
vantage-ola <vantageola4@gmail.com>
2024-12-24 14:20:59 +0100

    remove database mysql

--- database/007_create_nonces_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `nonces` (
-    `address` varchar(42) NOT NULL,
-    `nonce` int DEFAULT 0,
-    PRIMARY KEY (address)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
